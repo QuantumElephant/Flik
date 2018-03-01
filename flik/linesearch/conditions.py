8d7b63ed4ea555f1c80292f8e9085ec56f6a9af3
Taewon D. Kim <kimt33@mcmaster.ca>
2018-03-01 17:43:40 -0500

    Clean up strong wolfe
    1. Use check_input to test inputs
    2. Remove tests for input check

--- flik/linesearch/conditions.py
+++ flik/linesearch/conditions.py
@@ -1,84 +1,56 @@
 """Conditions for the line search algorithm."""
-
 import numpy as np
+from flik.linesearch.tools import check_input
 
 
 def strong_wolfe(grad, current_point, current_step, alpha, c_2=0.9):
-    """Check the validity of the strong Wolfe condition.
+    """Check check that the given alpha satisfies the strong Wolfe condition.
 
     Parameters
     ----------
     grad: callable
-        Gradient of the objective function
-    current_point: np.ndarray, float, int
-        Current iteration point
-        If a float or int are given they are converted to a np.ndarray
-    current_step: np.ndarray, float, int
-        Current direction along which the minimum will be searched
-        If a float or int are given they are converted to a np.ndarray
+        Gradient of the objective function.
+    current_point: {np.ndarray, float, int}
+        Current iteration point.
+        If a float or int is given, they are converted to a np.ndarray.
+    current_step: {np.ndarray, float, int}
+        Current direction along which the minimum will be searched.
+        If a float or int is given, they are converted to a np.ndarray.
     alpha: float
-        Step length
+        Step length.
     c_2: float
-        Strong Wolfe condition parameter
+        Strong Wolfe condition parameter.
 
     Raises
     -------
     TypeError
-        If grad is not a callable object
-        If current_point and current_step are not numpy arrays
-        If current_point and current_step are not 1-dimensional vectors
-        If the elements of current_point and current_step are not floats
-        If alpha and c_2 are not floats
+        If grad is not a callable object.
+        If current_point and current_step are not numpy arrays.
+        If current_point and current_step are not 1-dimensional vectors.
+        If the elements of current_point and current_step are not floats.
+        If alpha and c_2 are not floats.
     ValueError
-        If current_point and current_step have different sizes
-        If alpha is not in the interval (0,1]
-        If c_2 is not in the interval (0,1)
+        If current_point and current_step have different sizes.
+        If alpha is not in the interval (0,1].
+        If c_2 is not in the interval (0,1).
 
     Returns
     -------
-    bool:
+    bool
         True if the condition is satisfied, False otherwise
 
     """
-    if not callable(grad):
-        raise TypeError("The gradient should be a function")
-
-    if isinstance(current_point, (float, int)):
-        current_point = np.array([current_point], dtype=float)
-    elif not isinstance(current_point, np.ndarray):
-        raise TypeError("Current point should be a numpy array, float, or int")
-
-    if isinstance(current_step, (float, int)):
-        current_step = np.array([current_step], dtype=float)
-    elif not isinstance(current_step, np.ndarray):
-        raise TypeError("Current step should be a numpy array, float, or int")
-
-    if current_point.ndim != 1:
-        raise TypeError("Current point should be given as a 1-dimensional vector")
-
-    if current_step.ndim != 1:
-        raise TypeError("Current step should be given as a 1-dimensional vector")
-
-    if current_point.dtype != float:
-        raise TypeError("Current point should be given as a numpy array of floats")
-
-    if current_step.dtype != float:
-        raise TypeError("Current step should be given as a numpy array of floats")
-
-    if not isinstance(alpha, float):
-        raise TypeError("Alpha should be a float")
-
+    check_input(grad=grad, var=current_point, direction=current_step, alpha=alpha)
     if not isinstance(c_2, float):
         raise TypeError("c_2 should be a float")
-
-    if current_point.size != current_step.size:
-        raise ValueError("Current point and current step should ve vectors of the same size")
-
-    if not 0.0 < alpha <= 1.0:
-        raise ValueError("Alpha should be in the interval (0,1]")
-
     if not 0.0 < c_2 < 1.0:
         raise ValueError("c_2 should be in the interval (0,1)")
 
-    return (abs(current_step.dot(grad(current_point+alpha*current_step)))
-            <= c_2*abs(current_step.dot(grad(current_point))))
+    if isinstance(current_point, (int, float)):
+        current_point = np.array(current_point, dtype=float, ndmin=1)
+    if isinstance(current_step, (int, float)):
+        current_step = np.array(current_step, dtype=float, ndmin=1)
+
+    current_grad = grad(current_point)
+    next_grad = grad(current_point + alpha*current_step)
+    return abs(current_step.dot(next_grad)) <= c_2*abs(current_step.dot(current_grad))
--- flik/linesearch/test/test_conditions.py
+++ flik/linesearch/test/test_conditions.py
@@ -1,61 +1,37 @@
 """Tests for flik.linesearch.conditions."""
 
 import numpy as np
-
 from nose.tools import assert_raises
 from flik.linesearch.conditions import strong_wolfe
 
 
 def test_strong_wolfe():
     """Test for the strong_wolfe function."""
-    # Set parameters
-
     def grad(current_point):
-        """Gradient of function=current_point**2."""
+        """Gradient of function :math:`current_point**2`."""
         return 2 * current_point
 
+    # Set parameters
     current_point = np.array([1.8])
     current_step = np.array([-0.6])
     alpha = 0.8
 
-    # Checking input quality
-
-    grad_w = "This is not callable"
-    assert_raises(TypeError, strong_wolfe, grad_w, current_point, current_step, alpha)
-    current_point_w = "This is not a numpy array, float, or int"
-    assert_raises(TypeError, strong_wolfe, grad, current_point_w, current_step, alpha)
-    current_step_w = "This is not a numpy array, float, or int"
-    assert_raises(TypeError, strong_wolfe, grad, current_point, current_step_w, alpha)
-    current_point_w = np.array([[1., 2.], [3., 4.]])
-    assert_raises(TypeError, strong_wolfe, grad, current_point_w, current_step, alpha)
-    current_step_w = np.array([[1., 2.], [3., 4.]])
-    assert_raises(TypeError, strong_wolfe, grad, current_point, current_step_w, alpha)
-    current_point_w = np.array([1])
-    assert_raises(TypeError, strong_wolfe, grad, current_point_w, current_step, alpha)
-    current_step_w = np.array([2])
-    assert_raises(TypeError, strong_wolfe, grad, current_point, current_step_w, alpha)
-    alpha_w = 2
-    assert_raises(TypeError, strong_wolfe, grad, current_point, current_step, alpha_w)
     c_2_w = 1
     assert_raises(TypeError, strong_wolfe, grad, current_point, current_step, alpha, c_2_w)
-    current_point_w = np.array([1., 2.])
-    current_step_w = np.array([0.9])
-    assert_raises(ValueError, strong_wolfe, grad, current_point_w, current_step_w, alpha)
-    alpha_w = 1.4142
-    assert_raises(ValueError, strong_wolfe, grad, current_point, current_step, alpha_w)
     c_2_w = -0.209
     assert_raises(ValueError, strong_wolfe, grad, current_point, current_step, alpha, c_2_w)
+    c_2_w = 0.9
 
     # Making current_point and current_step float and int
     current_point_float = 1.8
-    assert strong_wolfe(grad, current_point_float, current_step, alpha)
+    assert strong_wolfe(grad, current_point_float, current_step, alpha, c_2_w)
     current_point_int = 2
-    assert strong_wolfe(grad, current_point_int, current_step, alpha)
+    assert strong_wolfe(grad, current_point_int, current_step, alpha, c_2_w)
     current_step_float = -0.6
-    assert strong_wolfe(grad, current_point, current_step_float, alpha)
+    assert strong_wolfe(grad, current_point, current_step_float, alpha, c_2_w)
     current_step_int = -1
-    assert strong_wolfe(grad, current_point, current_step_int, alpha)
+    assert strong_wolfe(grad, current_point, current_step_int, alpha, c_2_w)
 
     # Checking condition
-    assert strong_wolfe(grad, current_point, current_step, alpha)
-    assert not strong_wolfe(grad, current_point, -current_step, alpha)
+    assert strong_wolfe(grad, current_point, current_step, alpha, c_2_w)
+    assert not strong_wolfe(grad, current_point, -current_step, alpha, c_2_w)
