6bb4031d53e9eefbaa92f7ad0c2d59904e1883f8
Taewon D. Kim <kimt33@mcmaster.ca>
2018-03-01 18:56:01 -0500

    Clean up cubic interpolation
    1. Change API
    2. Update check_input
    3. Add more flexibility for alphas (can be equal to interval)

--- flik/linesearch/interpolations.py
+++ flik/linesearch/interpolations.py
@@ -1,48 +1,40 @@
 """Interpolations for step length selection."""
 import numpy as np
+from flik.linesearch.tools import check_input
 
-from flik.linesearch.input import check_input
 
-
-def cubic_interpolation(var, func, grad, direction, interval_begin, interval_end,
-                        alpha_previous, alpha_current):
+def cubic_interpolation(var, func, grad, direction, alpha_prev, alpha_current,
+                        interval_begin=0, interval_end=1):
     r"""Find a next step length for functions where the gradient is not expensive to calculate.
-
-    References
-    ----------
-    This cubic interpolation algorithm is taken from equation (3.59) in "Numerical Optimization" by
-    Nocedal and Wright, Second Edition (2006).
-
 
     Parameters
     ----------
-    var: np.array((N,1), dtype=float)
-        Variables' current values
+    var: {np.array(N,), float, int}
+        Variables' current values.
     func: callable
-        Function :math:`f(x)`
+        Function :math:`f(x)`.
     grad: callable
-        Gradient of the function :math:`\nabla f(x)`
-    direction: np.ndarray((N,1)) for N variables
-        Direction vector for next step
-    interval_begin: float
-        The starting point of the interval containing desirable step lengths
-    interval_end: float
-        The ending point of the interval containing desirable step lengths
-    alpha_previous: float
+        Gradient of the function :math:`\nabla f(x)`.
+    direction: {np.ndarray(N,), float, int}
+        Direction vector for next step.
+    alpha_prev: float
         The (i-1)-th step length estimate
     alpha_current: float
         The i-th step length estimate
+    interval_begin: {float, 0}
+        Starting point of the interval containing desirable step lengths.
+    interval_end: {float, 1}
+        End point of the interval containing desirable step lengths
 
     Raises
     ------
-        TypeError
-            according to the function check_input
-        ValueError
-            according to the function check_input
-            if interval_begin and interval_end are not such that
-                0 < interval_begin < interval_end <= 1
-            if alpha_1 and alpha_2 are not such that
-                interval_begin < alpha_previous < alpha_current < interval_end
+    TypeError
+        If interval_begin is not an int or float
+        If interval_end is not an int or float
+        If not 0 <= interval_begin < interval_end <= 1
+    ValueError
+        If not 0 < interval_begin < interval_end <= 1.
+        If not interval_begin < alpha_prev < alpha_current < interval_end.
 
     Returns
     -------
@@ -50,47 +42,53 @@ def cubic_interpolation(var, func, grad, direction, interval_begin, interval_end
         The minimizer :math:`\alpha` of :math:`f(x + \alpha p)`, where alpha_next is in the provided
         interval [interval_begin, interval_end]
 
+    References
+    ----------
+    This cubic interpolation algorithm is taken from equation (3.59) in "Numerical Optimization" by
+    Nocedal and Wright, Second Edition (2006).
+
     """
     # Check the input arguments
-    #   TODO: modify check_input function to accept more than one alpha value
-    check_input(var, func, grad, direction, alpha_previous)
-    check_input(var, func, grad, direction, alpha_current)
+    check_input(var=var, func=func, grad=grad, direction=direction, alpha=alpha_prev)
+    check_input(alpha=alpha_current)
 
-    #   Check for 0 < interval_begin < interval_end <= 1
-    if not (0.0 < interval_begin < interval_end <= 1.0):
-        raise ValueError("interval_begin and interval_end should be according to"
-                         " 0 < interval_begin < interval_end <= 1")
-
-    #   Check for interval_begin < alpha_1 < alpha_2 < interval_end
-    if not (interval_begin < alpha_previous < alpha_current < interval_end):
-        raise ValueError("alpha_previous and alpha_current should be according"
-                         " interval_begin < alpha_previous < alpha_current < interval_end")
+    # Check intervals and alphas
+    if not isinstance(interval_begin, (int, float)):
+        raise TypeError('interval_begin must be an int or a float.')
+    if not isinstance(interval_end, (int, float)):
+        raise TypeError('interval_end must be an int or a float.')
+    if not 0 <= interval_begin < interval_end <= 1:
+        raise ValueError("interval_begin and interval_end must satisfy "
+                         "0 <= interval_begin < interval_end <= 1")
+    if not interval_begin <= alpha_prev <= interval_end:
+        raise ValueError("alpha_prev must satisfy interval_begin <= alpha_prev <= interval_end.")
+    if not interval_begin <= alpha_current <= interval_end:
+        raise ValueError("alpha_current must satisfy interval_begin <= alpha_current <= "
+                         "interval_end.")
 
     # Define the helper functions phi and its derivative
-    def phi(alpha):
+    def phi_current(alpha):
         """Calculate the function value at (var + alpha * direction)."""
         return func(var + alpha * direction)
 
-    def phi_(alpha):
+    def phi_prev(alpha):
         """Calculate the derivative of the previously defined function phi."""
         return grad(var + alpha * direction).dot(direction)
 
     # If interval_begin or interval_end are not minimizers, then the minimizer lies in the interval
-    # ]interval_begin, interval_end[
-    d_1 = phi_(alpha_previous) + phi_(alpha_current) - 3 * (phi(alpha_previous) -
-                                                            phi(alpha_current)) / (alpha_previous -
-                                                                                   alpha_current)
+    # [interval_begin, interval_end]
+    d_1 = (phi_prev(alpha_prev) + phi_prev(alpha_current) - 3 *
+           (phi_current(alpha_prev) - phi_current(alpha_current)) / (alpha_prev - alpha_current))
 
-    d_2 = np.sign(alpha_current - alpha_previous) * np.sqrt(d_1 ** 2 - phi_(alpha_previous) *
-                                                            phi_(alpha_current))
+    d_2 = (np.sign(alpha_current - alpha_prev) *
+           np.sqrt(d_1 ** 2 - phi_prev(alpha_prev) * phi_prev(alpha_current)))
 
-    alpha_next = alpha_current - (alpha_current - alpha_previous) * (phi_(alpha_current) + d_2 -
-                                                                     d_1) / (phi_(alpha_current) -
-                                                                             phi_(alpha_previous) +
-                                                                             2 * d_2)
+    alpha_next = (alpha_current -
+                  (alpha_current - alpha_prev) * (phi_prev(alpha_current) + d_2 - d_1) /
+                  (phi_prev(alpha_current) - phi_prev(alpha_prev) + 2*d_2))
 
     # Return the minimizer of phi. It can be a, b, or the calculated value for alpha_next
-    d = {interval_begin: phi(interval_begin), interval_end: phi(interval_end),
-         alpha_next: phi(alpha_next)}
+    d = {interval_begin: phi_current(interval_begin), interval_end: phi_current(interval_end),
+         alpha_next: phi_current(alpha_next)}
 
     return min(d, key=d.get)
--- flik/linesearch/test/test_interpolations.py
+++ flik/linesearch/test/test_interpolations.py
@@ -2,86 +2,37 @@
 
 import numpy as np
 from nose.tools import assert_raises
-
 from flik.linesearch.interpolations import cubic_interpolation
 
 
-def test_cubic_interpolation_input():
-    """Test the checking of the arguments for the cubic_interpolation function."""
-    # We need some prototype correct arguments for the function
-    var = np.array([0.1, 2.0])
-
-    def func(x):
-        return x.dot(x)
-
-    def grad(x):
-        return 2 * x
-
-    direction = np.array([-1.0, 0])
-
-    # Check raises for parameters that aren't in the check_input function
-    #   alphas should be floats
-    interval_start = 0.2
-    interval_end = 0.8
-
-    alpha_1 = "a"
-    alpha_2 = 0.7
-    assert_raises(TypeError, cubic_interpolation, var, func, grad, direction, interval_start,
-                  interval_end, alpha_1, alpha_2)
-
-    alpha_1 = 0.43
-    alpha_2 = (2.3, 0.2)
-    assert_raises(TypeError, cubic_interpolation, var, func, grad, direction, interval_start,
-                  interval_end, alpha_1, alpha_2)
-
-    #   0 < interval_start < interval_end <= 1
-    alpha_1 = 0.3
-    alpha_2 = 0.6
-
-    interval_start = -1.0
-    interval_end = 0.8
-    assert_raises(ValueError, cubic_interpolation, var, func, grad, direction, interval_start,
-                  interval_end, alpha_1, alpha_2)
-
-    interval_start = 0.5
-    interval_end = 2.0
-    assert_raises(ValueError, cubic_interpolation, var, func, grad, direction, interval_start,
-                  interval_end, alpha_1, alpha_2)
-
-    #   interval_start < alpha_1 < alpha_2 < interval_end
-    interval_start = 0.2
-    interval_end = 0.8
-
-    alpha_1 = 0.1
-    alpha_2 = 0.7
-    assert_raises(ValueError, cubic_interpolation, var, func, grad, direction, interval_start,
-                  interval_end, alpha_1, alpha_2)
-
-    alpha_1 = 0.3
-    alpha_2 = 0.9
-    assert_raises(ValueError, cubic_interpolation, var, func, grad, direction, interval_start,
-                  interval_end, alpha_1, alpha_2)
-
-
 def test_cubic_interpolation():
-    """Test the cubic interpolation algorithm with a manual calculation."""
-    # Let's pick the following function: f(x) = x.x
+    """Test the for the cubic_interpolation function with bijection."""
     def func(x):
+        r"""Return value of function :math:`f(x)=x^T x`."""
         return x.dot(x)
 
     def grad(x):
+        r"""Return gradient of function :math:`\nabla f(x)=2x`."""
         return 2 * x
 
-    # We pick an initial vector and a corresponding descent direction
     var = np.array([0.0, -1.0, 2.0])
     direction = np.array([2.0, 1.0, 0.0])
-
-    # In the interval
     interval_start = 0.01
     interval_end = 1.0
-    # with initial guesses for alpha_1 and alpha_2 being
     alpha_1 = 0.1
     alpha_2 = 0.9
+
+    # Check raises for parameters that aren't in the check_input function
+    assert_raises(TypeError, cubic_interpolation, var, func, grad, direction, 0, 0.9, '0', 1)
+    assert_raises(TypeError, cubic_interpolation, var, func, grad, direction, 0, 0.9, 0, '1')
+    assert_raises(ValueError, cubic_interpolation, var, func, grad, direction, 0, 0.9, 0.1, 0.9)
+    assert_raises(ValueError, cubic_interpolation, var, func, grad, direction, 0.1, 1, 0.1, 0.9)
+    assert_raises(ValueError, cubic_interpolation, var, func, grad, direction, 1, 0.9, 0.1, 0.9)
+    assert_raises(ValueError, cubic_interpolation, var, func, grad, direction, 0.1, 0, 0.1, 0.9)
+    assert_raises(ValueError, cubic_interpolation, var, func, grad, direction, 0, 1, 0, 2)
+    assert_raises(ValueError, cubic_interpolation, var, func, grad, direction, 0, 1, -1, 1)
+    assert_raises(ValueError, cubic_interpolation, var, func, grad, direction, 0.1, 0.9, 1, 0)
+
     # a manual calculation shows that the return of the function should be
-    assert np.abs(cubic_interpolation(var, func, grad, direction, interval_start, interval_end,
-                                      alpha_1, alpha_2) - 0.2) < 1.0e-8
+    assert np.abs(cubic_interpolation(var, func, grad, direction, alpha_1, alpha_2,
+                                      interval_start, interval_end) - 0.2) < 1e-8
