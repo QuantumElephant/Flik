25c0294a4264db432a1eb3d53f8aee38f5b7627a
Taewon D. Kim <kimt33@mcmaster.ca>
2018-03-01 20:48:24 -0500

    Clean up soft wolfe
    1. Merge strong wolfe and soft wolfe as a single function, distinguished
       from one another with a keyword argument
    2. Merge tests for strong and soft wolfe

--- flik/linesearch/conditions.py
+++ flik/linesearch/conditions.py
@@ -3,23 +3,26 @@ import numpy as np
 from flik.linesearch.tools import check_input
 
 
-def strong_wolfe(grad, current_point, current_step, alpha, c_2=0.9):
-    """Check check that the given alpha satisfies the strong Wolfe condition.
+def wolfe(grad, current_point, current_step, alpha, const2=0.9, strong_wolfe=True):
+    """Check if the given alpha satisfies the Wolfe condition.
 
     Parameters
     ----------
     grad: callable
         Gradient of the objective function.
-    current_point: {np.ndarray, float, int}
+    current_point: {np.ndarray(N,), float, int}
         Current iteration point.
         If a float or int is given, they are converted to a np.ndarray.
-    current_step: {np.ndarray, float, int}
-        Current direction along which the minimum will be searched.
+    current_step: {np.ndarray(N,), float, int}
+        Descent direction.
         If a float or int is given, they are converted to a np.ndarray.
     alpha: float
         Step length.
-    c_2: float
-        Strong Wolfe condition parameter.
+    const2: {float, 0.9}
+        Condition parameter.
+    strong_wolfe: {bool, False}
+        If False, then checks for soft Wolfe condition.
+        Default is strong Wolfe condition.
 
     Raises
     -------
@@ -28,11 +31,11 @@ def strong_wolfe(grad, current_point, current_step, alpha, c_2=0.9):
         If current_point and current_step are not numpy arrays.
         If current_point and current_step are not 1-dimensional vectors.
         If the elements of current_point and current_step are not floats.
-        If alpha and c_2 are not floats.
+        If alpha and const2 are not floats.
     ValueError
         If current_point and current_step have different sizes.
-        If alpha is not in the interval (0,1].
-        If c_2 is not in the interval (0,1).
+        If alpha is not in the interval [0,1].
+        If const2 is not in the interval (0,1).
 
     Returns
     -------
@@ -41,69 +44,20 @@ def strong_wolfe(grad, current_point, current_step, alpha, c_2=0.9):
 
     """
     check_input(grad=grad, var=current_point, direction=current_step, alpha=alpha)
-    if not isinstance(c_2, float):
-        raise TypeError("c_2 should be a float")
-    if not 0.0 < c_2 < 1.0:
-        raise ValueError("c_2 should be in the interval (0,1)")
+    if not isinstance(const2, float):
+        raise TypeError("const2 should be a float")
+    if not 0.0 < const2 < 1.0:
+        raise ValueError("const2 should be in the interval (0, 1)")
 
     if isinstance(current_point, (int, float)):
         current_point = np.array(current_point, dtype=float, ndmin=1)
     if isinstance(current_step, (int, float)):
         current_step = np.array(current_step, dtype=float, ndmin=1)
 
-    current_grad = grad(current_point)
-    next_grad = grad(current_point + alpha*current_step)
-    return abs(current_step.dot(next_grad)) <= c_2*abs(current_step.dot(current_grad))
+    left = current_step.dot(grad(current_point + alpha * current_step))
+    right = const2 * current_step.dot(grad(current_point))
 
-
-def soft_wolfe(grad, val, alpha, direction, const2=0.9):
-    """
-    Soft-wolfe (curvature) condition.
-
-    Parameters
-    ----------
-    grad: callable
-        Gradient of objective nonlinear function
-    val: np.ndarray(N,)
-        Objective function value
-    alpha: float
-        Step length
-    direction: np.ndarray(N,)
-        Descent direction
-    const2: float
-        Constant C2
-
-    Raises
-    ------
-    ValueError
-        If C2 is not between 0 and 1
-    TypeError
-        If gradient function is not a function
-        If gradient function does not return 1-dim array
-
-    Returns
-    -------
-    bool
-        True if curvature condition is satisfied
-
-    """
-    # Check parameters
-    if not (isinstance(val, np.ndarray) and val.ndim == 1):
-        raise TypeError('Variable should be a 1-dim array of float')
-    if not (isinstance(direction, np.ndarray) and direction.ndim == 1):
-        raise TypeError('Descent direction should be a 1-dim array of float')
-    if not isinstance(alpha, float):
-        raise TypeError('Alpha should be a float')
-    if val.size != direction.size:
-        raise ValueError('Size of direction and variable should be same')
-    if not 0 < const2 < 1:
-        raise ValueError('C2 is not within boundary')
-    if not callable(grad):
-        raise TypeError('Gradient function should be a function')
-    if not isinstance(grad(val), np.ndarray):
-        raise TypeError('Gradient of function should return 1-dim array')
-
-    left_con = direction.dot(grad(val + alpha * direction))
-    right_con = const2 * direction.dot(grad(val))
-
-    return left_con >= right_con
+    if strong_wolfe:
+        return abs(left) <= abs(right)
+    else:
+        return left >= right
--- flik/linesearch/test/test_conditions.py
+++ flik/linesearch/test/test_conditions.py
@@ -1,12 +1,11 @@
 """Tests for flik.linesearch.conditions."""
 import numpy as np
 from numpy.testing import assert_raises
-from flik.linesearch.conditions import strong_wolfe
-from flik.linesearch.conditions import soft_wolfe
+from flik.linesearch.conditions import wolfe
 
 
-def test_strong_wolfe():
-    """Test for the strong_wolfe function."""
+def test_wolfe():
+    """Test the wolfe condition."""
     def grad(current_point):
         """Gradient of function :math:`current_point**2`."""
         return 2 * current_point
@@ -15,59 +14,29 @@ def test_strong_wolfe():
     current_point = np.array([1.8])
     current_step = np.array([-0.6])
     alpha = 0.8
+    const2 = 0.9
 
-    c_2_w = 1
-    assert_raises(TypeError, strong_wolfe, grad, current_point, current_step, alpha, c_2_w)
-    c_2_w = -0.209
-    assert_raises(ValueError, strong_wolfe, grad, current_point, current_step, alpha, c_2_w)
-    c_2_w = 0.9
+    const2_w = 1
+    assert_raises(TypeError, wolfe, grad, current_point, current_step, alpha, const2_w)
+    const2_w = -0.209
+    assert_raises(ValueError, wolfe, grad, current_point, current_step, alpha, const2_w)
 
     # Making current_point and current_step float and int
     current_point_float = 1.8
-    assert strong_wolfe(grad, current_point_float, current_step, alpha, c_2_w)
+    assert wolfe(grad, current_point_float, current_step, alpha, const2)
     current_point_int = 2
-    assert strong_wolfe(grad, current_point_int, current_step, alpha, c_2_w)
+    assert wolfe(grad, current_point_int, current_step, alpha, const2)
     current_step_float = -0.6
-    assert strong_wolfe(grad, current_point, current_step_float, alpha, c_2_w)
+    assert wolfe(grad, current_point, current_step_float, alpha, const2)
     current_step_int = -1
-    assert strong_wolfe(grad, current_point, current_step_int, alpha, c_2_w)
+    assert wolfe(grad, current_point, current_step_int, alpha, const2)
 
     # Checking condition
-    assert strong_wolfe(grad, current_point, current_step, alpha, c_2_w)
-    assert not strong_wolfe(grad, current_point, -current_step, alpha, c_2_w)
+    assert wolfe(grad, current_point, current_step, alpha, const2, strong_wolfe=True)
+    assert not wolfe(grad, current_point, -current_step, alpha, const2, strong_wolfe=True)
 
-
-def test_soft_wolfe():
-    """Test soft wolfe/curvature condition."""
-    # Set up parameters
-    def grad(val):
-        """Right gradient of objective function. Returns 1-dim array."""
-        return 2 * val
-
-    direction = np.array([2., 1., 3.])
+    current_point = np.array([2., 1., 3.])
+    current_step = np.array([2., 1., 3.])
     alpha = 0.1
-    val = np.array([2., 1., 3.])
-
-    # Check input quality
-    val_w = [1., 2., 3.]
-    assert_raises(TypeError, soft_wolfe, grad, val_w, alpha, direction)
-    grad_w = 2
-    assert_raises(TypeError, soft_wolfe, grad_w, val, alpha, direction)
-    alpha_w = 1
-    assert_raises(TypeError, soft_wolfe, grad, val, alpha_w, direction)
-    direction_w = [2., 1., 3.]
-    assert_raises(TypeError, soft_wolfe, grad, val, alpha, direction_w)
-    direction_w = np.array([2., 1.])
-    assert_raises(ValueError, soft_wolfe, grad, val, alpha, direction_w)
-    const2_w = 1.1
-    assert_raises(ValueError, soft_wolfe, grad, val, alpha, direction, const2_w)
-
-    def grad_ww(val):
-        """Wrong gradient of objective function. Returns scalar."""
-        return val.dot(val)
-
-    assert_raises(TypeError, soft_wolfe, grad_ww, val, alpha, direction)
-
-    # Check condition
-    assert soft_wolfe(grad, val, alpha, direction)
-    assert not soft_wolfe(grad, val, alpha, -direction)
+    assert wolfe(grad, current_point, current_step, alpha, const2, strong_wolfe=False)
+    assert not wolfe(grad, current_point, -current_step, alpha, const2, strong_wolfe=False)
